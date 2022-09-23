97edf02aeda372184536188b13ffb6222cf8754e
Cadranel-Kael <kael.cadranel00@gmail.com>
2022-09-23 17:05:49 +0200

    commit updateexercice10

--- starter/script010.js
+++ starter/script010.js
@@ -3,8 +3,13 @@
 */
 
 // 1. Déclarer les variables now, ageJohn, ageMark, yearJohn et yearMark
-// puis affecter aux trois premières les valeurs 2019, 28 et 33
+// puis affecter aux trois premières les valeurs 2022, 28 et 33
 
+let ageJohn, ageMark, yearJohn, yearMark;
+
+const now = 2022;
+ageJohn = 28;
+ageMark = 33;
 
 // Opérateurs mathématiques
 
@@ -14,17 +19,27 @@
 // puis afficher la date de naissance de John 
 // et "Mark est né en …"
 
+yearJohn = now - ageJohn; //C'est une expression
+console.log(`John est née en ${yearJohn}`);
+
+yearMark = now - ageMark;
+console.log(`Mark est née en ${yearMark}`);
 
 // 3. Afficher l'année à laquelle nous serons dans deux ans
 // le double de l'année
 // le dixième de l'année
 
+console.log(`Dans deux ans nous serons en ${now+2}`)
+console.log(`Dans dans 2x ${now} : ${now*2}`)
+console.log(`Dans dans 1/10 de ${now} : ${now/10}`)
 
-
-// Opérateurs logiques
+// Opérateurs logiques (<, >, <=, >=, &&, ||(pipe))
 
 // 4. …
 
+console.log(`Mark est-il née après 2021 ? ${yearMark>2021}`);
+
+console.log(typeof(1 + 1));
 
 
 // Opérateur typeof
