cd3ee3d382d7198dda271b1f9d012d734567e633
Cadranel-Kael <kael.cadranel00@gmail.com>
2022-09-23 15:48:07 +0200

    commit :sparkles:

--- starter/script008.js
+++ starter/script008.js
@@ -4,21 +4,38 @@
 
 // 1. Initialiser une variable firstName à John et afficher sa valeur dans la console
 
+let firstName = "John";
+
+console.log(firstName);
 
 // 2. Initialiser une variable lastName à Smith et une variable age à 28
 
+let lastName = "Smith";
+let age = 28;
+
+console.log(firstName,lastName,age);
 
 // 3. Initialiser une variable fullAge à true et l'afficher
 
+let fullAge = true;
+
+console.log(firstName,lastName,age,fullAge);
 
 // 4. Déclarer une variable job et afficher sa valeur dans la console
 
+let job; //Declaration sans infectation
+job = "étudiant";
+
+console.log(job);
 
 // 5. Affecter la valeur Enseignat à la variable job et l'afficher 
 
+job = "étudiant";
+
+console.log(job);
 
 // 6. Règles de nommage des variables : le nom ne peut pas 
 // - commencer par un chiffre
 // - contenir de caractères spéciaux
-// - être un mot réservé du langage
+// - être un mot réservé du langage <-- Camel case
 
